fdb583a6f627e0f37e5dc59374e9607f92939c7f
hanyk <hanyk9656@gmail.com>
2022-06-08 09:50:47 +0900

    JSX 정리

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,19 @@
+import { Fragment } from 'react';
 import './app.css';
 
 function App() {
+  const name = 'yk';
   return (
-    <h1>Hello :)</h1>
+    <Fragment>
+      <h1>Hello :)</h1>
+      {name &&  <h1>Hi {name}</h1>}
+      {
+        ['🎈', '🍋'].map(item => 
+        <h1>{item}</h1>)
+      }
+     
+    </Fragment>
+    
   );
 }
 
