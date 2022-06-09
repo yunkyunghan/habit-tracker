89d169edf27097c2d9e4979a8376ed9da422fa12
hanyk <hanyk9656@gmail.com>
2022-06-09 15:30:06 +0900

    State 만들기

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,24 +1,44 @@
 import React, { Component } from 'react'
 
 class Habit extends Component {
-  render() {
-    return (
-        <li className="habit">
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">8</span>
-            <button className="habit-button habit-increase">
-                <i className="fa-solid fa-plus"></i>
-            </button>
-            <button className="habit-button habit-decrease">
-                <i className="fa-solid fa-minus"></i>
-            </button>
-            <button className="habit-button habit-delete">
-                <i className="fa-solid fa-trash"></i>
-            </button>
+    state = {
+        count: 0,
+    };
 
-        </li>
-        ); 
-    }
+    handleIncrement = () => {
+        // state 오브젝트 안에 있는 카운트를 증가 한 뒤 state를 업데이트 해야 함.
+        this.setState({count: this.state.count + 1});
+    };
+
+    handleDecrement = () => {
+        const count = this.state.count -1
+        this.setState({count: count < 0 ? 0 : count});
+    };
+
+    render() {
+        return (
+            <li className="habit">
+                <span className="habit-name">Coding</span>
+                <span className="habit-count">{this.state.count}</span>
+                <button 
+                    className="habit-button habit-increase" 
+                    onClick={this.handleIncrement}
+                >
+                    <i className="fa-solid fa-plus"></i>
+                </button>
+                <button 
+                    className="habit-button habit-decrease"
+                    onClick={this.handleDecrement}
+                >
+                    <i className="fa-solid fa-minus"></i>
+                </button>
+                <button className="habit-button habit-delete">
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+
+            </li>
+            ); 
+        }
 }
 
 export default Habit;
