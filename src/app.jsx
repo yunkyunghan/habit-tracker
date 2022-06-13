3652c49560752e19bda5629168127dd752027ee7
hanyk <hanyk9656@gmail.com>
2022-06-13 11:54:29 +0900

    navbar 컴포넌트 만들기

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
 import './app.css';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
+import React, { Component } from 'react';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+        {id: 1, name: 'Coding', count: 0},
+        {id: 2, name: 'Drinking Coffee', count: 0},
+        {id: 3, name: 'Working out', count: 0}
+    ]
+};
+
+handleIncrement = (habit) => {
+  const _habits = [...this.state.habits];
+  const index = _habits.indexOf(habit);
+
+  _habits[index].count++;
+  this.setState( { habits: _habits } ); // key, value 값이 같으면 key값만 써줘도 무방.
+};
+
+handleDecrement = (habit) => {
+  const _habits = [...this.state.habits];
+  const index = _habits.indexOf(habit);
+  const count = _habits[index].count - 1;
+
+  _habits[index].count = count < 0 ? 0 : count;   
+  this.setState( { habits: _habits } );
+};
+
+handleDelete = (habit) => {
+  const _habits = this.state.habits.filter(item => item.id !== habit.id);
+
+  this.setState( { habits: _habits } );
+};
+
+  render() {
+    return (
+      <>
+        <Navbar 
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        />
+
+        <Habits 
+        habits = {this.state.habits} 
+        onIncrement={this.handleIncrement} 
+        onDecrement={this.handleDecrement} 
+        onDelete={this.handleDelete}  
+      />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,42 +2,23 @@ import React, { Component } from 'react'
 import Habit from './habit';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            {id: 1, name: 'Coding', count: 0},
-            {id: 2, name: 'Drinking Coffee', count: 0},
-            {id: 3, name: 'Working out', count: 0}
-        ]
-    };
 
     handleIncrement = (habit) => {
-        const _habits = [...this.state.habits];
-        const index = _habits.indexOf(habit);
-
-        _habits[index].count++;
-        this.setState( { habits: _habits } ); // key, value 값이 같으면 key값만 써줘도 무방.
+        this.props.onIncrement(habit);
     };
 
     handleDecrement = (habit) => {
-        const _habits = [...this.state.habits];
-        const index = _habits.indexOf(habit);
-        const count = _habits[index].count - 1;
-
-        _habits[index].count = count < 0 ? 0 : count;   
-        this.setState( { habits: _habits } );
+        this.props.onDecrement(habit);
     };
 
     handleDelete = (habit) => {
-        const _habits = this.state.habits.filter(item => item.id !== habit.id);
-
-        this.setState( {habits: _habits } );
+        this.props.onDelete(habit);
     };
 
     render() {
         return (
             <ul>
-                {
-                    this.state.habits.map(myHabit => (
+                {this.props.habits.map(myHabit => (
                         <Habit 
                             key={myHabit.id} 
                             habitt={myHabit} 
@@ -45,8 +26,7 @@ class Habits extends Component {
                             onDecrement={this.handleDecrement} 
                             onDelete={this.handleDelete} 
                         />
-                    ))
-                }
+                    ))}
             </ul>
         )
     }
