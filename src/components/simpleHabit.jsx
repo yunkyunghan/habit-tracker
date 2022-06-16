e2c22185be14750311a498e764fff0c537828f4e
hanyk <hanyk9656@gmail.com>
2022-06-16 17:18:12 +0900

    React Hook 테스트

---
+++ src/components/simpleHabit.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useRef, useCallback, useEffect } from "react";
+
+const SimpleHabit = () => {
+    const [count, setCount] = useState(0);
+    const spanRef = useRef();
+   
+    const handleIncrement = useCallback(() => {
+        setCount(count + 1);
+    },[count]);
+
+    useEffect(() => {
+        console.log(`mounted & updated: ${count}`)
+    })
+    return (
+        <li className="habit">
+            <span ref={spanRef} className="habit-name">Reading</span>
+            <span className="habit-count">{count}</span>
+            <button 
+                className="habit-button habit-increase"
+                onClick={handleIncrement}
+            >
+                <i className="fa-solid fa-plus"></i>  
+            </button>
+        </li>
+    );
+};
+            
+export default SimpleHabit;
+
